882f4ef59a2b0964acc77894e0b279ef0f6dd902
UriProtocol <angel_3041210085@utd.edu.mx>
2023-03-15 12:24:26 -0600

    modulo PTCs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 var cors = require('cors')
 const bcrypt = require('bcrypt')
 const fileUpload = require('express-fileupload')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = 5000
@@ -17,7 +18,7 @@ app.use(fileUpload())
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'tutorias',
+    database: 'tutorias2',
     password: ''
 })
 
@@ -315,8 +316,39 @@ app.get('/profesor/acceder', (req, res) =>{
             )
         }
         if(result.length > 0){
-            res.send({
-                mensaje: 'Holas'
+            bcrypt.compare(password, result[0].password, (error1, result1) =>{
+                if(error1){
+                    return(
+                        res.send({
+                            status: 102,
+                            auth: false,
+                            error1,
+                            mensaje: 'Problemas con el servidor - contacte al administrador'
+                        })
+                    )
+                }
+                if(result1){
+                    const clave = result[0].clave
+                    const nombres = result[0].nombres
+                    const apellidos = result[0].apellidos
+                    const estatus = result[0].estatus
+                    const foto = result[0].foto
+                    const token = jwt.sign({clave}, myPlainTextPassword, {
+                        expiresIn:"1 day",
+                    })
+
+
+
+                    return(
+                        res.send({
+                            status: 200,
+                            auth: true,
+                            resultado: {clave, nombres, apellidos, estatus, foto, token},
+                            mensaje: 'Datos correctos'
+                            
+                        })
+                    )
+                }
             })
         }else{
             res.send({
@@ -328,6 +360,132 @@ app.get('/profesor/acceder', (req, res) =>{
     })
 
 
+})
+
+// -----------Actividades PTC-----------
+
+app.post('/actividades/agregar/:clave', (req, res)=>{
+    const {
+        fecha,
+        clases,
+        duracion,
+        materias,
+        grupos,
+        alumnos,
+        horario,
+        preparacion,
+        revision,
+        investigacion,
+        administrativas,
+    } = req.body
+    const {clave} = req.params
+
+    const sql = 'INSERT INTO actividades values(?,?,?,?,?,?,?,?,?,?,?,?,?)'
+
+    db.query(sql, ['NULL',fecha, clases, materias, grupos, alumnos, horario, duracion, preparacion, revision, investigacion, administrativas, clave], (err, result) =>{
+        if(err){
+            res.send({
+                status: 100,
+                errNo: err.errno,
+                mensaje: err.message,
+                codigo: err.code
+            })
+        } else{
+            res.send({
+                status: 200
+            })
+        }
+    })
+
+})
+
+app.patch('/actividades/modificar/:id', (req, res)=>{
+    const {
+        clases,
+        duracion,
+        materias,
+        grupos,
+        alumnos,
+        horario,
+        preparacion,
+        revision,
+        investigacion,
+        administrativas,
+    } = req.body
+    const {id} = req.params
+
+    const sql = 'UPDATE actividades SET clases=?, duracion=?, materias=?, grupos=?, alumnos=?, horario=?, preparacion=?, revision=?, investigacion=?, administrativas=? WHERE id=?'
+
+    db.query(sql, [clases, duracion, materias, grupos, alumnos, horario, preparacion, revision, investigacion, administrativas, id], (err, result) =>{
+        if(err){
+            res.send({
+                status: 100,
+                errNo: err.errno,
+                mensaje: err.message,
+                codigo: err.code
+            })
+        } else{
+            res.send({
+                status: 200
+            })
+        }
+    })
+
+})
+
+app.get('/actividades/ver/:clave', (req,res) =>{
+    const {clave} = req.params
+    const sql = 'SELECT * FROM actividades WHERE clave_profesor = ?'
+    db.query(sql, [clave], (err, result) =>{
+        if(!err){
+            res.send({
+                status: 200,
+                result,
+            })
+        }else{
+                res.send({
+                    status: 400,
+                    result: {}
+                })
+            }
+      })
+})
+
+
+app.get('/actividades/ver/registro/:id', (req,res) =>{
+    const {id} = req.params
+    const sql = 'SELECT * FROM actividades WHERE id = ?'
+    db.query(sql, [id], (err, result) =>{
+        if(!err){
+            res.send({
+                status: 200,
+                result,
+            })
+        }else{
+                res.send({
+                    status: 400,
+                    result: {}
+                })
+            }
+      })
+})
+
+app.delete('/actividades/eliminar/:id', (req,res) =>{
+    const {id} = req.params
+    const sql = 'DELETE FROM actividades WHERE id = ?'
+    db.query(sql, [id], (err, result) =>{
+        if(!err){
+            res.send({
+                status: 200,
+                result,
+            })
+        }else{
+                res.send({
+                    status: 400,
+                    result: {}
+                })
+            }
+      })
 })
 
 
